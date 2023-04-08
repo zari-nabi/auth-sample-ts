4561fd812e2970bae5ef5d4d8d3416158600550a
Zahra Mohammadnabi <z.mohammadnabi@fanap.ir>
2023-04-08 18:43:49 +0430

    complete Navebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 import AllRoutes from './routes/AllRouts';
 
 function App() {
   return (
     <>
-      <AllRoutes/>
+      <Navbar />
+      <AllRoutes />
     </>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,32 @@
-const Navbar = ()=>{
-    return <></>
+
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Context/authContext";
+import "./Navbar.style.scss";
+
+const Navbar = () => {
+    const { isAuthenticated,handleLogout } = useContext(AuthContext);
+    return (
+        <div className="navbar">
+            <div className="navbar__left">
+                <Link to="/" className="link">Home</Link>
+                {isAuthenticated ?
+                    <>
+                        <Link to="/users" className="link">Dashboard</Link>
+                        <Link to="/users" className="link">Users</Link>
+                    </>
+                    : null
+                }
+            </div>
+            <div className="navbar__right">
+                {isAuthenticated ?
+                    <a onClick={handleLogout} className="link">Logout</a>
+                    :
+                    <Link to="/login" className="link">Login</Link>
+                }
+            </div>
+        </div>
+    )
 };
 
 export default Navbar;
